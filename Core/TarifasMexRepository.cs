24bba5cff5d1546ba9cdccecb64b285c7ff0d9ef
PedroV200 <astepedro@gmail.com>
2023-11-17 10:02:35 -0300

    Se invierte el query de DESC a ASC en tarifasMex

--- Core/TarifasMexRepository.cs
+++ Core/TarifasMexRepository.cs
@@ -103,7 +103,7 @@ public class TarifasMexRepository : ITarifasMexRepository
 
     public async Task<IEnumerable<TarifasMex>> GetAllAsync()
     {
-        var sql = $"SELECT * FROM tarifasmex ORDER BY abs(extract(epoch from (fecha - timestamp '{DateTime.Now.ToString("yyyy-MM-dd")}'))) DESC";
+        var sql = $"SELECT * FROM tarifasmex ORDER BY abs(extract(epoch from (fecha - timestamp '{DateTime.Now.ToString("yyyy-MM-dd")}'))) ASC";
         using (var connection = new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection")))
         {
             connection.Open();
